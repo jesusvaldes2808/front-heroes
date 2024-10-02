fc0d13de814adbe513f9136a0cab763cf89e470b
jesusvaldes2808 <jesus.valdes@cetus.com.co>
2024-10-02 11:25:56 -0500

    env

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -3,6 +3,6 @@ const server = {
 }
 export const environments = {
   production: true,
-  urlBase: `${server.protocol}://localhost:8081/api/v1/superhero`,
-  urlBackend: `${server.protocol}://localhost:8081/api/v1`
+  urlBase: `${server.protocol}://172.30.1.230:8081/api/v1/superhero`,
+  urlBackend: `${server.protocol}://172.30.1.230:8081/api/v1`
 }
